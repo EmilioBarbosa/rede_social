6115ba40104b3e39cc7b844679978c93e447bf11
Emilio Barbosa <emiliobarbos4@gmail.com>
2022-07-03 23:53:03 -0300

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
         LEFT JOIN amigos as a ON a.id_solicitante = {$id} 
         INNER JOIN users as us ON us.id = p.user_id 
         WHERE a.id_solicitado = p.user_id OR a.id_solicitante = p.user_id
-        GROUP BY p.id, us.id
+        
         ");   
             
         $Profiles = Profile::all();
